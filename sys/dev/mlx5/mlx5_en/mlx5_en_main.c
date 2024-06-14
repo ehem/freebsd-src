6d9f991031af2ab2f2ee7c05b65b73606d203a67
Elliott Mitchell <ehem+freebsd@m5p.com>
2024-06-14 16:10:40 -0700

    intrtab: move header #includes to preferred places
    Since sys/intrtab.h depends on machine/machintr.h, these files should
    really be #including sys/intrtab.h, rather than machine/machintr.h.

--- sys/dev/mlx5/mlx5_en/mlx5_en_main.c
+++ sys/dev/mlx5/mlx5_en/mlx5_en_main.c
@@ -32,9 +32,9 @@
 #include <dev/mlx5/mlx5_en/en.h>
 
 #include <sys/eventhandler.h>
+#include <sys/intrtab.h>
 #include <sys/sockio.h>
 #include <machine/atomic.h>
-#include <machine/machintr.h>
 
 #include <net/debugnet.h>
 #include <netinet/tcp_ratelimit.h>
