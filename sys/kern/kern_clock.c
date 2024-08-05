ehem/freebsd-src

commit aba10e131fe7a9ea168af9ff9002e95559a2f80b

Allow swi_sched() to be called from NMI context.
For purposes of handling hardware error reported via NMIs I need a way to
escape NMI context, being too restrictive to do something significant.

To do it this change introduces new swi_sched() flag SWI_FROMNMI, making
it careful about used KPIs.  On platforms allowing IPI sending from NMI
context (x86 for now) it immediately wakes clk_intr_event via new IPI_SWI,
otherwise it works just like SWI_DELAY.  To handle the delayed SWIs this
patch calls clk_intr_event on every hardclock() tick.

MFC after:	2 weeks
Sponsored by:	iXsystems, Inc.
Differential Revision:	https://reviews.freebsd.org/D25754

sys/kern/kern_clock.c
@@ -508,6 +508,7 @@ hardclock(int cnt, int usermode)
 			if (i > 0 && i <= newticks)
 				watchdog_fire();
 		}
+		intr_event_handle(clk_intr_event, NULL);
 	}
 	if (curcpu == CPU_FIRST())
 		cpu_tick_calibration();

sys/kern/kern_intr.c
@@ -60,6 +60,7 @@ __FBSDID("$FreeBSD$");
 #include <machine/atomic.h>
 #include <machine/cpu.h>
 #include <machine/md_var.h>
+#include <machine/smp.h>
 #include <machine/stdarg.h>
 #ifdef DDB
 #include <ddb/ddb.h>
@@ -85,6 +86,7 @@ struct	intr_entropy {
 	uintptr_t event;
 };
 
+struct	intr_event *clk_intr_event;
 struct	intr_event *tty_intr_event;
 void	*vm_ih;
 struct proc *intrproc;
@@ -1018,7 +1020,7 @@ swi_add(struct intr_event **eventp, const char *name, driver_intr_t handler,
 	    void *arg, int pri, enum intr_type flags, void **cookiep)
 {
 	struct intr_event *ie;
-	int error;
+	int error = 0;
 
 	if (flags & INTR_ENTROPY)
 		return (EINVAL);
@@ -1036,8 +1038,10 @@ swi_add(struct intr_event **eventp, const char *name, driver_intr_t handler,
 		if (eventp != NULL)
 			*eventp = ie;
 	}
-	error = intr_event_add_handler(ie, name, NULL, handler, arg,
-	    PI_SWI(pri), flags, cookiep);
+	if (handler != NULL) {
+		error = intr_event_add_handler(ie, name, NULL, handler, arg,
+		    PI_SWI(pri), flags, cookiep);
+	}
 	return (error);
 }
 
@@ -1055,9 +1059,11 @@ swi_sched(void *cookie, int flags)
 	CTR3(KTR_INTR, "swi_sched: %s %s need=%d", ie->ie_name, ih->ih_name,
 	    ih->ih_need);
 
-	entropy.event = (uintptr_t)ih;
-	entropy.td = curthread;
-	random_harvest_queue(&entropy, sizeof(entropy), RANDOM_SWI);
+	if ((flags & SWI_FROMNMI) == 0) {
+		entropy.event = (uintptr_t)ih;
+		entropy.td = curthread;
+		random_harvest_queue(&entropy, sizeof(entropy), RANDOM_SWI);
+	}
 
 	/*
 	 * Set ih_need for this handler so that if the ithread is already
@@ -1066,7 +1072,16 @@ swi_sched(void *cookie, int flags)
 	 */
 	ih->ih_need = 1;
 
-	if (!(flags & SWI_DELAY)) {
+	if (flags & SWI_DELAY)
+		return;
+
+	if (flags & SWI_FROMNMI) {
+#if defined(SMP) && (defined(__i386__) || defined(__amd64__))
+		KASSERT(ie == clk_intr_event,
+		    ("SWI_FROMNMI used not with clk_intr_event"));
+		ipi_self_from_nmi(IPI_SWI);
+#endif
+	} else {
 		VM_CNT_INC(v_soft);
 		error = intr_event_schedule_thread(ie);
 		KASSERT(error == 0, ("stray software interrupt"));
@@ -1346,6 +1361,8 @@ intr_event_handle(struct intr_event *ie, struct trapframe *frame)
 	CK_SLIST_FOREACH(ih, &ie->ie_handlers, ih_next) {
 		if ((ih->ih_flags & IH_SUSP) != 0)
 			continue;
+		if ((ie->ie_flags & IE_SOFT) != 0 && ih->ih_need == 0)
+			continue;
 		if (ih->ih_filter == NULL) {
 			thread = true;
 			continue;
@@ -1570,6 +1587,9 @@ static void
 start_softintr(void *dummy)
 {
 
+	if (swi_add(&clk_intr_event, "clk", NULL, NULL, SWI_CLOCK,
+	    INTR_MPSAFE, NULL))
+		panic("died while creating clk swi ithread");
 	if (swi_add(NULL, "vm", swi_vm, NULL, SWI_VM, INTR_MPSAFE, &vm_ih))
 		panic("died while creating vm swi ithread");
 }

sys/x86/x86/mp_x86.c
@@ -47,6 +47,7 @@ __FBSDID("$FreeBSD$");
 #ifdef GPROF 
 #include <sys/gmon.h>
 #endif
+#include <sys/interrupt.h>
 #include <sys/kdb.h>
 #include <sys/kernel.h>
 #include <sys/ktr.h>
@@ -1618,6 +1619,16 @@ cpususpend_handler(void)
 	CPU_CLR_ATOMIC(cpu, &resuming_cpus);
 	CPU_CLR_ATOMIC(cpu, &suspended_cpus);
 	CPU_CLR_ATOMIC(cpu, &toresume_cpus);
+}
+
+/*
+ * Handle an IPI_SWI by waking delayed SWI thread.
+ */
+void
+ipi_swi_handler(struct trapframe frame)
+{
+
+	intr_event_handle(clk_intr_event, &frame);
 }
 
 /*

share/man/man9/swi.9
@@ -23,7 +23,7 @@
 .\"
 .\" $FreeBSD$
 .\"
-.Dd April 19, 2012
+.Dd July 25, 2020
 .Dt SWI 9
 .Os
 .Sh NAME
@@ -132,7 +132,7 @@ The
 .Fa flags
 argument specifies how and when the handler should be run and is a mask of one
 or more of the following flags:
-.Bl -tag -width SWI_DELAY
+.Bl -tag -width SWI_FROMNMI
 .It Dv SWI_DELAY
 Specifies that the kernel should mark the specified handler as needing to run,
 but the kernel should not schedule the software interrupt thread to run.
@@ -146,6 +146,13 @@ functionality performed by
 .Fn setdelayed
 in earlier versions of
 .Fx .
+.It Dv SWI_FROMNMI
+Specifies that
+.Fn swi_sched
+is called from NMI context and should be careful about used KPIs.
+On platforms allowing IPI sending from NMI context it immediately wakes
+.Va clk_intr_event
+via the IPI, otherwise it works just like SWI_DELAY.
 .El
 .Pp
 The

sys/amd64/amd64/apic_vector.S
@@ -206,6 +206,16 @@ IDTVEC(spuriousint)
 	jmp	doreti
 
 /*
+ * Executed by a CPU when it receives an IPI_SWI.
+ */
+	INTR_HANDLER ipi_swi
+	call	as_lapic_eoi
+	FAKE_MCOUNT(TF_RIP(%rsp))
+	call	ipi_swi_handler
+	MEXITCOUNT
+	jmp	doreti
+
+/*
  * Executed by a CPU when it receives a RENDEZVOUS IPI from another CPU.
  *
  * - Calls the generic rendezvous action function.

sys/amd64/amd64/mp_machdep.c
@@ -223,6 +223,10 @@ cpu_mp_start(void)
 	setidt(IPI_SUSPEND, pti ? IDTVEC(cpususpend_pti) : IDTVEC(cpususpend),
 	    SDT_SYSIGT, SEL_KPL, 0);
 
+	/* Install an IPI for calling delayed SWI */
+	setidt(IPI_SWI, pti ? IDTVEC(ipi_swi_pti) : IDTVEC(ipi_swi),
+	    SDT_SYSIGT, SEL_KPL, 0);
+
 	/* Set boot_cpu_id if needed. */
 	if (boot_cpu_id == -1) {
 		boot_cpu_id = PCPU_GET(apic_id);

sys/amd64/include/smp.h
@@ -32,6 +32,7 @@ inthand_t
 	IDTVEC(invlop_pti),
 	IDTVEC(invlop),
 	IDTVEC(ipi_intr_bitmap_handler_pti),
+	IDTVEC(ipi_swi_pti),
 	IDTVEC(cpustop_pti),
 	IDTVEC(cpususpend_pti),
 	IDTVEC(rendezvous_pti);

sys/i386/i386/apic_vector.s
@@ -309,6 +309,23 @@ IDTVEC(cpususpend)
 	jmp	doreti
 
 /*
+ * Executed by a CPU when it receives an IPI_SWI.
+ */
+	.text
+	SUPERALIGN_TEXT
+IDTVEC(ipi_swi)
+	PUSH_FRAME
+	SET_KERNEL_SREGS
+	cld
+	KENTER
+	call	as_lapic_eoi
+	FAKE_MCOUNT(TF_EIP(%esp))
+	movl	$ipi_swi_handler, %eax
+	call	*%eax
+	MEXITCOUNT
+	jmp	doreti
+
+/*
  * Executed by a CPU when it receives a RENDEZVOUS IPI from another CPU.
  *
  * - Calls the generic rendezvous action function.

commit 236d9bd715bccd9edf56be23d79fe6f9248154b4

kern: remove clk_intr_event from non-ACPI
clk_intr_event was added strictly for ACPI.  While additional uses
could be added in the future, presently this is ACPI-only.  Remove it
from non-ACPI kernels.

Correct the uses of intr_event_handle().  If no handlers are added by
ACPI, invoking intr_event_handle() is incorrect as it is stray.

Fixes: aba10e131fe ("Allow swi_sched() to be called from NMI context.")
Differential Revision: https://reviews.freebsd.org/D40776

sys/kern/kern_clock.c
@@ -40,6 +40,7 @@
 #include "opt_hwpmc_hooks.h"
 #include "opt_ntp.h"
 #include "opt_watchdog.h"
+#include "opt_acpi.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -88,7 +89,9 @@ static struct mtx time_lock;
 SDT_PROVIDER_DECLARE(sched);
 SDT_PROBE_DEFINE2(sched, , , tick, "struct thread *", "struct proc *");
 
+#ifdef DEV_ACPI
 struct	intr_event *clk_intr_event;
+#endif
 
 static int
 sysctl_kern_cp_time(SYSCTL_HANDLER_ARGS)
@@ -531,7 +534,10 @@ hardclock(int cnt, int usermode)
 			if (i > 0 && i <= newticks)
 				watchdog_fire();
 		}
-		intr_event_handle(clk_intr_event, NULL);
+#ifdef DEV_ACPI
+		if (!CK_SLIST_EMPTY(&clk_intr_event->ie_handlers))
+			intr_event_handle(clk_intr_event, NULL);
+#endif
 	}
 	if (curcpu == CPU_FIRST())
 		cpu_tick_calibration();
@@ -552,6 +558,7 @@ hardclock_sync(int cpu)
 /*
  * Start standard software interrupt threads
  */
+#ifdef DEV_ACPI
 static void
 start_softintr(void *dummy)
 {
@@ -565,6 +572,7 @@ start_softintr(void *dummy)
 }
 SYSINIT(start_softintr, SI_SUB_SOFTINTR, SI_ORDER_FIRST, start_softintr,
     NULL);
+#endif
 
 /*
  * Regular integer scaling formula without losing precision:

sys/kern/kern_intr.c
@@ -1134,9 +1134,13 @@ swi_sched(void *cookie, int flags)
 
 	if (flags & SWI_FROMNMI) {
 #if defined(SMP) && (defined(__i386__) || defined(__amd64__))
+#ifdef DEV_ACPI
 		KASSERT(ie == clk_intr_event,
 		    ("SWI_FROMNMI used not with clk_intr_event"));
 		ipi_self_from_nmi(IPI_SWI);
+#else
+		KASSERT(0, ("SWI_FROMNMI used not with clk_intr_event"));
+#endif
 #endif
 	} else {
 		VM_CNT_INC(v_soft);

sys/sys/clock.h
@@ -198,7 +198,9 @@ void clock_dbgprint_ts(device_t dev, int rw, const struct timespec *ts);
 /*
  * SWI for clock events
  */
+#ifdef DEV_ACPI
 extern struct	intr_event *clk_intr_event;
+#endif
 
 #endif /* _KERNEL */
 

sys/x86/x86/mp_x86.c
@@ -1696,7 +1696,10 @@ void
 ipi_swi_handler(struct trapframe frame)
 {
 
-	intr_event_handle(clk_intr_event, &frame);
+#ifdef DEV_ACPI
+	if (!CK_SLIST_EMPTY(&clk_intr_event->ie_handlers))
+		intr_event_handle(clk_intr_event, &frame);
+#endif
 }
 
 /*